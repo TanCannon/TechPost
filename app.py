cc4be72fc6b6d15f931c17e96f1e2cf94c3e41ad
Tanmaya Kumar Naik <tanmayakumarnaik2003@gmail.com>
2025-09-01 21:53:46 +0530

    Update app.py
    modified to get the whole from root to config.json path.

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ from werkzeug.utils import secure_filename
 import math
 
 '''we can use the json file here to add our own parameters'''
-with open('./config.json','r') as c: #reading from json file the urls
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(BASE_DIR, "config.json")
+print(f"CONFIG_PATH:{config_path}")
+with open(config_path,'r') as c: #reading from json file the urls
     params = json.load(c)['params']
 
 local_server = True 
